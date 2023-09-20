fba17f51df6ded31c43f5986a23bdbc08ebfd20b
hilarious3 <shahzad20022002@gmail.com>
2023-09-21 02:09:36 +0530

    http get method

--- model/user_model.py
+++ model/user_model.py
@@ -1,10 +1,17 @@
 import mysql.connector
+import json
 class user_model():
     def __init__(self):
         try:
-            con=mysql.connector.connect(host="localhost",user="root",password="password",database="flask",port="1080")
+            self.con=mysql.connector.connect(host="localhost",user="root",password="password",database="flask",port="1080")
+            self.cur=self.con.cursor(dictionary=True)
             print("connected__________!")
         except:
             print("some error")
     def user_signup_model(self):
-        return "hii this is model"
+        self.cur.execute("SELECT* FROM users")
+        result=self.cur.fetchall()
+        if len(result)>0:
+            return json.dumps(result)
+        else:
+            return "No Data Found"
