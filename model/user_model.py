1a9510b90f05272c10802e9e5e939cfdc15601d3
hilarious3 <shahzad20022002@gmail.com>
2023-09-21 01:49:25 +0530

    connecting to db python mysql connector

--- model/user_model.py
+++ model/user_model.py
@@ -1,4 +1,10 @@
+import mysql.connector
 class user_model():
+    def __init__(self):
+        try:
+            con=mysql.connector.connect(host="localhost",user="root",password="password",database="flask",port="1080")
+            print("connected__________!")
+        except:
+            print("some error")
     def user_signup_model(self):
         return "hii this is model"
-        
