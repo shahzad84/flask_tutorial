a80f06a6fe008533b1d8d78def78e3a362a232b5
hilarious3 <shahzad20022002@gmail.com>
2023-09-21 20:28:09 +0530

    http post method

--- controller/user_controller.py
+++ controller/user_controller.py
@@ -1,6 +1,11 @@
 from app import app
+from flask import request
 from model.user_model import user_model
 obj=user_model()
 @app.route("/user/signup")
 def signup():
     return obj.user_signup_model()
+@app.route("/user/addone",methods=["POST"])
+def addone():
+    # print(request.form)
+    return obj.user_addone_model(request.form)
--- model/user_model.py
+++ model/user_model.py
@@ -4,6 +4,7 @@ class user_model():
     def __init__(self):
         try:
             self.con=mysql.connector.connect(host="localhost",user="root",password="password",database="flask",port="1080")
+            self.con.autocommit=True
             self.cur=self.con.cursor(dictionary=True)
             print("connected__________!")
         except:
@@ -15,3 +16,6 @@ class user_model():
             return json.dumps(result)
         else:
             return "No Data Found"
+    def user_addone_model(self,data):
+        self.cur.execute(f"INSERT INTO users(name, email, phone, role, password) VALUES('{data['name']}', '{data['email']}', '{data['phone']}', '{data['role']}', '{data['password']}')")
+        return "user created sucessfully"
